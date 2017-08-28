6ba4527d78dd9e29eeb2656d0c2614d1acc342f5
juhog <juhog@869576.no-reply.drupal.org>
2017-08-28 16:00:53 -0400

    Issue #2905185 by juhog: Compatibility issue with Swift Mailer

--- src/Plugin/Mail/SendgridMail.php
+++ src/Plugin/Mail/SendgridMail.php
@@ -252,8 +252,14 @@ class SendGridMail implements MailInterface, ContainerFactoryPluginInterface {
               break;
 
             case 'text/html':
+              // Ensure body is a string before using it as HTML.
+              $body = $message['body'];
+              if ($body instanceof MarkupInterface) {
+                $body = $body->__toString();
+              }
+
               // The message includes only an HTML part.
-              $sendgrid_message->setHtml($message['body']);
+              $sendgrid_message->setHtml($body);
               // Also include a text only version of the email.
               $sendgrid_message->setText(MailFormatHelper::wrapMail(MailFormatHelper::htmlToText($message['body'])));
               break;
